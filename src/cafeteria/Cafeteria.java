b61b0f378925303f465b3f4f6b0b98e2dd5ae3e3
Oscar Bejar <oskrbejar@gmail.com>
2020-05-21 12:51:33 -0300

    test commit

--- src/cafeteria/Cafeteria.java
+++ src/cafeteria/Cafeteria.java
@@ -5,6 +5,7 @@ public class Cafeteria {
     public static void main(String[] args) {
         //Scanner in = new Scanner(System.in);
         //boolean repetidor = true;
+        //commit test
        
         Compra compra1 = new Compra();
          compra1.elegirBebida(); 
